ca361ebe25cb6e2d2850da21913cffa7e60317a6
keithwilson <kw15r@fsu.edu>
2024-11-21 14:50:10 -0500

    Added link for twilight op cards.

--- application.py
+++ application.py
@@ -1048,7 +1048,7 @@ def replace_aspects(text):
 
 
     keywords = ['Smuggle', 'Bounties', 'Ambush', 'Bounty', 'Overwhelm', 'Sentinel', 'Shielded', 'Raid 3', 'Saboteur', 'Grit',
-    'Restore 2', 'Restore 1', 'Raid 2', 'Exploit 2', 'Coordinate', 'Exploit 1', 'Exploit 3', 'Exploit 4']
+    'Restore 2', 'Restore 1', 'Raid 2', 'Exploit 2', 'Coordinate', 'Exploit 1', 'Exploit 3', 'Exploit 4', 'Raid 1', 'Restore 3']
 
     for keyword in keywords:
         placeholder = '{' + keyword + '}'
